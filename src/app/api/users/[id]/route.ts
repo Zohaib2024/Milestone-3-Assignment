da09414ce5ecb0f544b3b6e5cab75e30f007068f
Zohaib <azohaibshoukat22@gmail.com>
2025-01-01 10:07:59 +0500

    Your second commit message

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,12 +1,12 @@
-import { user } from "@/db";
-import { NextResponse } from "next/server";
+// import { user } from "@/db";
+// import { NextResponse } from "next/server";
 
-export function GET(request: any, content: any) {
-  const userData = user.filter((item) => item.id == content.params.id);
-  return NextResponse.json(
-    userData.length == 0
-      ? { result: "No Data Found", success: false }
-      : { result: userData[0], success: true },
-    { status: 200 }
-  );
-}
+// export function GET(request: any, content: any) {
+//   const userData = user.filter((item) => item.id == content.params.id);
+//   return NextResponse.json(
+//     userData.length == 0
+//       ? { result: "No Data Found", success: false }
+//       : { result: userData[0], success: true },
+//     { status: 200 }
+//   );
+// }
